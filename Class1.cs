bc340b9cb7a1fb129e5fcdb4ef39074d325d3bb5
Pruthvi RB <pruthvi94rb@gmail.com>
2023-09-01 07:04:02 +0530

    first commit

---
+++ Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using log4net;
+
+
+namespace TestProject
+{
+    public class Class1
+    {
+       static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+        }
+    }
+}
